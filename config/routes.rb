329b40b0e9f7afd4f74c38374148c81288182f35
Zachary Scott <mail@zzak.io>
2017-10-30 21:02:34 +0900

    Start adding stuff for signup and profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'users/new'
+  get '/signup', to: 'users#new'
   resources :games
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
