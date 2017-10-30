89514380d1c93a9eb266bbbcefede56ea40a0be2
Zachary Scott <mail@zzak.io>
2017-10-30 15:52:25 +0900

    rails g controller Home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   get 'users/new'
   resources :games
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'games#index'
+  root 'home#index'
 end
