9cc5948f7ce9bbab0a876aeaf3b6f00de10137e7
Zachary Scott <mail@zzak.io>
2017-10-31 14:29:39 +0900

    Add user#create system tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Save it
+      redirect_to user_path(id: @user.id)
     else
       render 'new'
     end
--- test/system/users_signups_test.rb
+++ test/system/users_signups_test.rb
@@ -1,9 +1,29 @@
 require "application_system_test_case"
 
 class UsersSignupsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_signups_url
-  #
-  #   assert_selector "h1", text: "UsersSignup"
-  # end
+  test "visit signup page" do
+    visit signup_url
+
+    assert_selector "h1", text: "Sign up"
+  end
+
+  test "password can't be blank" do
+    visit signup_url
+
+    click_on "Create my account"
+
+    assert_text "Password can't be blank"
+  end
+
+  test "signup goes to welcome user page" do
+    visit signup_url
+
+    fill_in "Email", with: "MyString@MyDomain"
+    fill_in "Password", with: "MyString"
+    fill_in "Confirmation", with: "MyString"
+
+    click_on "Create my account"
+
+    assert_text "Welcome, MyString@MyDomain"
+  end
 end
