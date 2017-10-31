733a7e3979b98d5781a6431feb0cf98ab5c5592b
Zachary Scott <mail@zzak.io>
2017-10-31 14:29:19 +0900

    Add authenticate_user but not tested for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  def authenticate_user
+    current_user
+    if @current_user
+      return true
+    else
+      redirect_to 'sessions#new'
+      return false
+    end
+  end
+
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
