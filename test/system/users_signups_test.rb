79f0ee0a526bbfd2589ff1880776647382156eb4
Zachary Scott <mail@zzak.io>
2017-10-31 13:19:06 +0900

    rails g system_test users_signup

---
+++ test/system/users_signups_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class UsersSignupsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit users_signups_url
+  #
+  #   assert_selector "h1", text: "UsersSignup"
+  # end
+end
